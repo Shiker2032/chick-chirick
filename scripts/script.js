9245a91d305c5a28a7a7567959f0d131d7d4185d
Vladislav <53581923+Shiker2032@users.noreply.github.com>
2022-02-02 21:53:45 +0300

    фикс десктопного и планшетного разрешения

--- scripts/script.js
+++ scripts/script.js
@@ -1,16 +1,16 @@
-sliderAlive = false;
- window.addEventListener("resize", () => {
-  if ((!sliderAlive) && (window.outerWidth <= 320)) {
-    $('.features').slick({
-      dots: true
-    });
+// sliderAlive = false;
+//  window.addEventListener("resize", () => {
+//   if ((!sliderAlive) && (window.outerWidth <= 320)) {
+//     $('.features').slick({
+//       dots: true
+//     });
 
-    $('.gallery__images').slick({
-      dots: true
-    });
-    sliderAlive = true;
-  }
-  else {
-    return
-  }
- });
+//     $('.gallery__images').slick({
+//       dots: true
+//     });
+//     sliderAlive = true;
+//   }
+//   else {
+//     return
+//   }
+//  });
