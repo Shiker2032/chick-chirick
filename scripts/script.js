5157c7fa73f21d886ea25a48b12186d019ceae49
Vladislav <53581923+Shiker2032@users.noreply.github.com>
2022-02-05 17:02:11 +0300

    Шрифты мобильной версии

--- scripts/script.js
+++ scripts/script.js
@@ -1,19 +1,19 @@
-// sliderAlive = false;
-//  window.addEventListener("resize", () => {
-//   if ((!sliderAlive) && (window.outerWidth <= 320)) {
-//     $('.features').slick({
-//       dots: true
-//     });
+sliderAlive = false;
+ window.addEventListener("resize", () => {
+  if ((!sliderAlive) && (window.outerWidth <= 320)) {
+    $('.features').slick({
+      dots: true
+    });
 
-//     $('.gallery__images').slick({
-//       dots: true
-//     });
-//     sliderAlive = true;
-//   }
-//   else {
-//     return
-//   }
-//  });
+    $('.gallery__images').slick({
+      dots: true
+    });
+    sliderAlive = true;
+  }
+  else {
+    return
+  }
+ });
 
 let textFields = document.querySelectorAll(".form__field");
 textFields.forEach((field) => {
